044177fb8b81fb2ea72aae33dab876fc50f288a3
MarconPL <marcon1993@gmail.com>
2016-03-07 11:22:56 +0100

    Test

--- GamePrototype/GamePrototype/Character.cs
+++ GamePrototype/GamePrototype/Character.cs
@@ -7,5 +7,13 @@ namespace GamePrototype
 {
     class Character
     {
+        string imie;
+        int sila;
+        int zrecznosc;
+        int kondycja;
+        int percepcja;
+        int inteligencja;
+        int sila_woli;
+        int charyzma;
     }
 }
